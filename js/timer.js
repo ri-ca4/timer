d2d08ffe74082da0960dbe6d00bd479a562e55c1
ri-ca4 <100642891+ri-ca4@users.noreply.github.com>
2022-04-11 07:44:49 -0400

    style framework, html structure, and add bootstrap

---
+++ js/timer.js
@@ -0,0 +1,8 @@
+/*
+    author: riley eyrich
+    date: 4/11/22
+    title: timer
+*/
+
+var interval = document.getElementById('interval').value;
+var breakTime = document.getElementById('break').value;
