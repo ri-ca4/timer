6f2eed228fbbf610837ffe3a6ec7fec8fd2ff5ce
ri-ca4 <riley.de12@gmail.com>
2022-04-25 14:59:39 -0400

    added reset function, fixed input and global var

--- js/timer.js
+++ js/timer.js
@@ -3,48 +3,52 @@
     date: 4/11/22
     title: timer
 */
-
-var intervalTime = document.getElementById('interval').value;
-var breakTime = document.getElementById('break').value;
+var intervalTime = '';
+var breakTime = '';
 
 var startBtn = document.getElementById('start');
 var pauseBtn = document.getElementById('pause');
 var resetBtn = document.getElementById('reset');
 
-startBtn.addEventListener('click', timerInt);
+startBtn.addEventListener('click', function(){
+    //validate input fields
+    intervalTime = document.getElementById('interval').value;
+    breakTime = document.getElementById('break').value;
+    timerInt();
+    }
+);
 
-pauseBtn.addEventListener('click', function(){
-    console.log('this did a thing');
-});
+//pause button event
 
 resetBtn.addEventListener('click', function(){
-    console.log('this did a thing');
-    document.getElementById('interval').value = 0;
-    document.getElementById('break').value = 0;
+    clearInterval(interval);
+    document.getElementById('clockFace').innerHTML = "";
 });
 
 function timerInt() {
-    mins = intervalTime;   
+    var mins = intervalTime;
+    document.getElementById('clockFace').innerHTML = mins;
     interval = setInterval(function() {  
          mins--;
          document.getElementById('clockFace').innerHTML = mins;
          if(!mins){
-              clearInterval(interval); 
-              alert("done");
-              timerBrk();
+            clearInterval(interval); 
+            alert("done");//play sound
+            timerBrk();
          }
     },1000)
 }
 
 function timerBrk() {
-    mins = breakTime;   
+    var mins = breakTime;
+    document.getElementById('clockFace').innerHTML = mins;
     interval = setInterval(function() {  
          mins--;
          document.getElementById('clockFace').innerHTML = mins;
          if(!mins){
-              clearInterval(interval); 
-              alert("done");
-              timerInt();
+            clearInterval(interval);
+            alert("done");//play sound
+            timerInt();
          }
     },1000)
 }
