8a07493e28c6d2f91adfb02f80abe15d76c5d029
ri-ca4 <riley.de12@gmail.com>
2022-04-25 13:30:36 -0400

    added event listeners for buttons

--- js/timer.js
+++ js/timer.js
@@ -6,3 +6,21 @@
 
 var interval = document.getElementById('interval').value;
 var breakTime = document.getElementById('break').value;
+
+var startBtn = document.getElementById('start');
+var pauseBtn = document.getElementById('pause');
+var resetBtn = document.getElementById('reset');
+
+startBtn.addEventListener('click', function(){
+    console.log('this did a thing');
+});
+
+pauseBtn.addEventListener('click', function(){
+    console.log('this did a thing');
+});
+
+resetBtn.addEventListener('click', function(){
+    console.log('this did a thing');
+    document.getElementById('interval').value = 0;
+    document.getElementById('break').value = 0;
+});
